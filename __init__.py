9c94cfefd142c8d3c7b62d81aa4fea05ff32656c
Michael Michel <michael.spi@web.de>
2023-02-12 16:11:30 +0100

    added functionality

--- __init__.py
+++ __init__.py
@@ -1,13 +1,22 @@
 from mycroft import MycroftSkill, intent_file_handler
+from pynput.keyboard import Key, Controller
+
 
 
 class Playpause(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
+        self.keyboard = Controller
 
-    @intent_file_handler('playpause.intent')
+    @intent_file_handler('play.intent')
     def handle_playpause(self, message):
-        self.speak_dialog('playpause')
+        self.keyboard.tap(Key.media_play_pause)
+        self.speak_dialog('play')
+
+    @intent_file_handler('pause.intent')
+    def handle_playpause(self, message):
+        self.keyboard.tap(Key.media_play_pause)
+        self.speak_dialog('pause')
 
 
 def create_skill():
